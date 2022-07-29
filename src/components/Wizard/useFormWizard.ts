ac1cbf31c86e005886657958f23f72083f41678b
rufflian <91517002+rufflian@users.noreply.github.com>
2022-07-29 16:58:38 +1000

    🐛 Prevent triggering complete at the start Also add bound checks

--- src/components/Wizard/useFormWizard.ts
+++ src/components/Wizard/useFormWizard.ts
@@ -10,16 +10,22 @@ export const useFormWizard = ({
   const [history, setHistory] = useState<string[]>([]);
   const [currentStep, setCurrentStep] = useState<number>(0);
   useEffect(() => {
-    if (currentStep === steps.length) {
+    if (currentStep && currentStep == steps.length) {
       complete();
     }
   }, [currentStep]);
   const goToPrevious = () => {
+    if (history.length === 0) {
+      throw new Error("No previous steps");
+    }
     setCurrentStep(steps.indexOf(history[history.length - 1]));
     history.pop();
     setHistory(history);
   };
   const goToNext = () => {
+    if (steps.length === currentStep) {
+      throw new Error("No more steps");
+    }
     setCurrentStep(currentStep + 1);
     setHistory([...history, steps[currentStep]]);
   };
