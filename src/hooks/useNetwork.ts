6c18e1cb6f71632cbe9ec2611cb6c98e405e7195
rufflian <91517002+rufflian@users.noreply.github.com>
2022-08-10 22:54:25 +1000

    ♻️ Add set network function to hook output

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -1,13 +1,22 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl } from "@solana/web3.js";
 import React, { useContext, useMemo } from "react";
 
-const NetworkContext = React.createContext<WalletAdapterNetwork>(WalletAdapterNetwork.Testnet);
+const NetworkContext = React.createContext<NetworkContextValue>({
+  network: WalletAdapterNetwork.Testnet
+});
 
-export const NetworkProvider = NetworkContext.Provider
-
-export default () => {
-    const network = useContext(NetworkContext)
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    return {network, endpoint}
+type NetworkContextValue = {
+    network: WalletAdapterNetwork,
+    setNetwork?: (arg: WalletAdapterNetwork) => void;
 }
+
+export const NetworkProvider = NetworkContext.Provider;
+
+const useNetwork = () => {
+  const {network, setNetwork} = useContext(NetworkContext);
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  return { network, endpoint, setNetwork };
+};
+
+export default useNetwork;
