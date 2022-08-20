c477b6c8fc919d550ee954fd7d77a2eab3c8c9f0
rufflian <91517002+rufflian@users.noreply.github.com>
2022-08-20 20:06:01 +1000

    ✨ Add explorer url util

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,6 +1,8 @@
 import { BN, web3 } from "@project-serum/anchor";
 import { Utils } from "@raindrops-protocol/raindrops";
 import { Connection } from "@solana/web3.js";
+import { Connection as ConnectionUtils } from "@raindrop-studios/sol-kit";
+import { getNetworkForWalletAdapter, Networks } from "./useNetwork";
 
 export const withCallback = async (asyncFunction: Function, callback: Function) => {
   const response = await asyncFunction();
@@ -22,4 +24,21 @@ export const getItemClassInfo = async (
   const [itemClassKey] = await Utils.PDA.getItemPDA(tokenMint, new BN(index));
   const accountInfo = await connection.getParsedAccountInfo(itemClassKey);
   return accountInfo;
+};
+
+export type ExplorerUrl = string | undefined
+
+export const getExplorerUrl = ({
+  txId,
+  network,
+}: {
+  txId: string;
+  network: Networks;
+}): ExplorerUrl => {
+  const networkFilter =
+    getNetworkForWalletAdapter(network) ||
+    `custom&customUrl=${ConnectionUtils.getClusterUrl(network)}`;
+  if (txId) {
+    return `https://explorer.solana.com/tx/${txId}?cluster=${networkFilter}`;
+  }
 };
